fcc1689860b41e5f1082f8526b19de9ea618b5a5
Kevin John <kevintylerjohn@gmail.com>
2023-10-02 15:10:32 -0700

    Bump version

--- Assets/Managers/AppLoader.cs
+++ Assets/Managers/AppLoader.cs
@@ -10,7 +10,7 @@ using System;
 //todo - app update absolutely does not work on macos, not sure if there's an easy way to do it
 public class AppLoader : MonoBehaviour
 {
-    public static int APP_VERSION = 89;
+    public static int APP_VERSION = 90;
 
     public GameObject[] localBundles;
     public ProgressBar loadingBar;
